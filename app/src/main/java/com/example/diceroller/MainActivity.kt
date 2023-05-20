bfabe9f14ff25d77a36c6fa4699709542bbd048b
Kaezr <inviktor27@gmail.com>
2023-05-20 21:37:43 +0530

    Started Developing

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -15,24 +15,20 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DiceRollerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
 
-                }
-            }
         }
     }
 }
 
 
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    DiceRollerTheme {
-    }
+fun DiceWithButtonAndImage() {
+
+}
+
+@Preview
+@Composable
+fun DiceRollerApp() {
+
 }
